057f0acda8242bb349d01d9d7b29807994bd6f46
juanchiigomezz <juanmanuelgomezomil@gmail.com>
2024-07-24 14:24:26 +0200

    chore: Setup Eslint and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // Aquí puedes agregar o modificar reglas según tus preferencias
+  },
 };
