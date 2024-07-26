63de3ae06ac4a3c59863da4324a87cfa205923a8
juanchiigomezz <juanmanuelgomezomil@gmail.com>
2024-07-27 00:22:27 +0200

    refactor: Update TabNavigation styling and add gradient background

--- src/navigation/TabNavigation.tsx
+++ src/navigation/TabNavigation.tsx
@@ -1,7 +1,8 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import LinearGradient from 'react-native-linear-gradient';
 import Home from '../screens/Home';
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import {faHome} from '@fortawesome/free-solid-svg-icons';
 import {useTheme} from 'styled-components/native';
 import LotusSVG from '../assets/svg/lotus';
 
@@ -19,10 +20,28 @@ const TabNavigation = () => {
       screenOptions={{
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: colors.primary.darker,
+          position: 'absolute',
+          bottom: 0,
+          left: 0,
+          right: 0,
+          elevation: 0,
+          backgroundColor: 'transparent',
           borderTopWidth: 0,
-          height: 50,
+          height: 60,
         },
+        tabBarBackground: () => (
+          <View style={StyleSheet.absoluteFill}>
+            <LinearGradient
+              colors={[
+                'rgba(0,0,0,0)', // Completely transparent
+                colors.primary.darker, // Solid color
+                colors.primary.darker, // Solid color (repeated to ensure full coverage)
+              ]}
+              locations={[0, 0.2, 1]} // 20% gradient, 80% solid
+              style={[StyleSheet.absoluteFill]}
+            />
+          </View>
+        ),
         tabBarLabelStyle: {
           fontFamily: typography.fontFamily.medium,
           fontSize: typography.fontSize.xs,
@@ -34,7 +53,7 @@ const TabNavigation = () => {
         component={Home}
         options={{
           tabBarIcon: () => <LotusSVG width={50} color={colors.text} />,
-          tabBarShowLabel: false,
+          // tabBarShowLabel: false,
         }}
       />
     </Tab.Navigator>
