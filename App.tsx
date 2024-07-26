9c8eca5bef94aacd4a8a2ddf1dd478c1d90ccf3f
juanchiigomezz <juanmanuelgomezomil@gmail.com>
2024-07-26 15:26:30 +0200

    refactor: Setup TrackPlayer and Add useTrackPlayerInit hook for initializing TrackPlayer

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import MainNavigation from './src/navigation/MainNavigation';
 import {client, ApolloProvider} from './src/apollo/client';
 import {ThemeProvider} from 'styled-components';
 import theme from './src/theme';
+import useTrackPlayerInit from './src/hooks/useTrackPlayerInit';
 
 const App = () => {
+  const {isLoading} = useTrackPlayerInit();
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
